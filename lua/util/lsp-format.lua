28184edbdd3b17ae9e6a5eccb48e9baac69ffd0b
Sebastian <sebastian.neumair@gmail.com>
2023-11-19 22:07:14 +0100

    format rust with rust-analyzer instead of efm

--- lua/util/lsp-format.lua
+++ lua/util/lsp-format.lua
@@ -1,13 +1,14 @@
 local M = {}
 
 M.format = function()
+	local rust = vim.lsp.get_active_clients({ name = "rust_analyzer" })
 	local efm = vim.lsp.get_active_clients({ name = "efm" })
 
-	if vim.tbl_isempty(efm) then
-		return
+	if not vim.tbl_isempty(rust) then
+		vim.lsp.buf.format({ name = "rust_analyzer", async = true })
+	elseif not vim.tbl_isempty(efm) then
+		vim.lsp.buf.format({ name = "efm", async = true })
 	end
-
-	vim.lsp.buf.format({ name = "efm", async = true })
 end
 
 return M
